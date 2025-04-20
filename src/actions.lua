2af90f0c2019902f01f6f2f437a184878eefd32d
GlebEvemi <gleb2466@gmail.com>
2025-04-21 00:03:36 +0300

    add actions

---
+++ src/actions.lua
@@ -0,0 +1,17 @@
+actions = {
+    ["1"] = function()
+        print("Selected: Check all tasks")
+        
+    end,
+    ["2"] = function()
+        print("Selected: Add task")
+        
+    end,
+    ["3"] = function()
+        print("Exiting the program...")
+        os.exit()
+    end
+}
+
+
+return actions
--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,22 @@
 
-
 local input
+local actions = require("src.actions")
 
 
 
 while true do
+    print("--------------------------------------")
+    print("1. Check all tasks")
+    print("2. Add task")
+    print("3. Exit")
+    io.write("> ")
     input = io.read()
-    if input == "" and input == nil then
-        break
-    else 
-        print("gay")
+
+    local action = actions[input]
+    if action then
+        action()
+    else
+        print("Bad Input")
     end
+    
 end
