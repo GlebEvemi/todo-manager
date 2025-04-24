3e569eb1741c79146ecc72a484334679dd596883
GlebEvemi <gleb2466@gmail.com>
2025-04-25 01:57:23 +0300

    Made changes in Action[2] func, now the program saves task into the tasks.lua

--- data/tasks.lua
+++ data/tasks.lua
@@ -1,7 +1,5 @@
-tasks = {
-    {text = "Learn Lua", status = "In progress"},
-    {text = "Learn math", status = "In progress"}
+return {
+  { text = "Learn Lua", status = "In progress" },
+  { text = "Learn math", status = "In progress" },
+  { text = "learn how to love", status = "WIP" },
 }
-
-
-return tasks
--- src/actions.lua
+++ src/actions.lua
@@ -11,7 +11,31 @@ actions = {
         
     end,
     ["2"] = function()
+        local file = io.open("data/tasks.lua", "w")
+        if file == nil then
+            return 
+        end
         print("Selected: Add task")
+        local text, status
+        print("Write the task description")
+        io.write("> ")
+        text = io.read()
+        print("Write the status")
+        io.write(">")
+        status = io.read()
+        table.insert(tasks, { text = text, status = status })
+
+        file:write("return {\n")
+        for _, task in ipairs(tasks) do
+        file:write(string.format(
+            "  { text = %q, status = %q },\n",
+            task.text, task.status
+        ))
+    end
+    file:write("}\n")
+    file:close()
+
+    print("Task saved successfully!")
         
     end,
     ["3"] = function()
